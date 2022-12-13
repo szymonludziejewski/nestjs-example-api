fcfb1b087ccc799f6d89c4bc689137044fb77f28
szymonludziejewski <szymon.ludziejewski@vizlib.com>
2022-12-13 10:02:48 +0100

    3 files modified

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+
+  const config = new DocumentBuilder()
+    .setTitle('Pets example')
+    .setDescription('Pets example')
+    .setVersion('1.0')
+    .addTag('pets')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
